9fdb9175b1cf3d906d7a88402928dbb112051560
Serhx4 <111274705+Serhx4@users.noreply.github.com>
2022-11-03 15:31:58 +0200

    Initial commit

---
+++ src/main/java/GitTeamwork.java
@@ -0,0 +1,15 @@
+public class GitTeamwork {
+
+    public static void main(String[] args) {
+        System.out.println("""
+        
+        Hi there! Welcome to the team!
+        ------------------------------
+        """);
+        simulateTeamwork("Serhx4");
+    }
+
+    private static void simulateTeamwork(String name) {
+        System.out.format("%s simulates teamwork :)\n", name);
+    }
+}
