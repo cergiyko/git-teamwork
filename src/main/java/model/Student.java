2ac20cac60f4f4b24689d31f98a129008ee97f12
Kirill <Duskmage2009@gmail.com>
2022-11-05 16:55:14 +0200

    ColorHair was added

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -7,7 +7,8 @@ public class Student {
 
     private Long id;
     private String name;
-
+    private String colorHair="Yellow";
+            
     public void sayAnything() {
         System.out.println("I'm student number 5");
     }
