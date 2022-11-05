df2dd3950372631ae0219a4fb8f2560c59a09a26
Ivan4112 <ivanbondar224@gmail.com>
2022-11-05 17:59:03 +0200

    new value (age) in Stundent

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -8,9 +8,10 @@ public class Student {
     private Long id;
     private String name;
     private String colorHair="Yellow";
-            
+    private int age = 0;
     public void sayAnything() {
-        System.out.println("I'm student number 5");
+        System.out.println("I'm student number 6");
     }
+
 
 }
